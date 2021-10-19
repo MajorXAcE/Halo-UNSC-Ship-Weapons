c035106d549f6e5d75fc8a055ca6570d97cfacfb
MajorXAcE <majorxace@hotmail.com>
2021-10-19 01:59:58 -0400

    Integrity Adjustments

--- Data/Scripts/CoreParts/SMAC.cs
+++ Data/Scripts/CoreParts/SMAC.cs
@@ -24,7 +24,7 @@ namespace Scripts
                         MuzzlePartId = "None",
                         AzimuthPartId = "None",
                         ElevationPartId = "None",
-                        DurabilityMod = .75f,
+                        //DurabilityMod = .75f,
                         IconName = ""
                     },
                 },
